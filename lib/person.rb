27a4478dafa24363b865efcd4e50b1697bdf5d29
Nolan Hughes <nhughes987@gmail.com>
2017-11-17 00:10:56 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,8 +7,8 @@ class Person
 end
 
 susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
- 
+
 susan = Person.new(susan_attributes)
-susan.name      # => "Susan"
-susan.height    # => "5'11""
-susan.eye_color # => "Green"
+# susan.name      # => "Susan"
+# susan.height    # => "5'11""
+# susan.eye_color # => "Green"
