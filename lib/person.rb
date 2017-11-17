9ac7dcbd4fe699e87c70c733538970701e875c74
Nolan Hughes <nhughes987@gmail.com>
2017-11-17 00:09:41 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,6 @@ class Person
   attr_accessor
 
   def initialize(attributes)
-    attribute_hash = attributes.each { |key, value| self.send(('#{key}='), value)}
+    attribute_hash = attributes.each { |key, value| self.send(("#{key}="), value)}
   end
 end
